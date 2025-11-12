8c8aab723924bfb54287bde6c96d732a3625d041
kapishan-2005 <kapishan.official@gmail.com>
2025-11-12 22:22:47 +0530

    changes

--- day2.js
+++ day2.js
@@ -1,78 +1,90 @@
 
 
-// number
-console.log(Number.MAX_VALUE*2);
-// min_value
-console.log(Number.MIN_VALUE);
-// nun
-console.log(Number.POSITIVE_INFINITY);
-// (5e-4= 5*10index -4)
-console.log(Number.EPSILON);
+// // number
+// console.log(Number.MAX_VALUE*2);
+// // min_value
+// console.log(Number.MIN_VALUE);
+// // nun
+// console.log(Number.POSITIVE_INFINITY);
+// // (5e-4= 5*10index -4)
+// console.log(Number.EPSILON);
 
-let num1=5 ;
+// let num1=5 ;
 
-let customerSpentAmount= [450,80,90,20];
-let totalAmountSpent= customerSpentAmount.reduce((prevVal,currentVal) => {
-    console.log(prevVal+currentVal)
-    return prevVal+currentVal
-},0);
-console.log(totalAmountSpent);
+// let customerSpentAmount= [450,80,90,20];
+// let totalAmountSpent= customerSpentAmount.reduce((prevVal,currentVal) => {
+//     console.log(prevVal+currentVal)
+//     return prevVal+currentVal
+// },0);
+// console.log(totalAmountSpent);
 
-let namee="saii";
-console.log(namee.length);
+// let namee="saii";
+// console.log(namee.length);
 
-x= namee.toUpperCase();
-console.log(x);
+// x= namee.toUpperCase();
+// console.log(x);
 
-let uname="jeru";
-let lname="thuva";
+// let uname="jeru";
+// let lname="thuva";
 
-let n =`hello, ${uname}${lname}! welcome to the uki`;
-console.log(n);
-// charector act
-let g="kapishan";
-console.log(g.charAt(5));
+// let n =`hello, ${uname}${lname}! welcome to the uki`;
+// console.log(n);
+// // charector act
+// let g="kapishan";
+// console.log(g.charAt(5));
 
-let foods=["noodels","chicken_rice","briyani","kottu"];
-console.log(foods.slice(2));
-console.log(foods.slice(2,3));
-console.log(foods.slice(2,-1));
+// let foods=["noodels","chicken_rice","briyani","kottu"];
+// console.log(foods.slice(2));
+// console.log(foods.slice(2,3));
+// console.log(foods.slice(2,-1));
 
-let nam="   kapi   ";
-console.log(nam);
-console.log(nam.trim());
-// template literals($)
-// string interpolation
+// let nam="   kapi   ";
+// console.log(nam);
+// console.log(nam.trim());
+// // template literals($)
+// // string interpolation
 
-let t= 20;
-console.log(Boolean(t));
+// let t= 20;
+// console.log(Boolean(t));
 
-let c=5;
-console.log(Boolean(c==8));
+// let c=5;
+// console.log(Boolean(c==8));
 
-let cc=60;
-let v=50;
-console.log(cc<v);
-
-
-
-let cusName= prompt("Enter Your Name");
-let price = prompt("Enter the price");
+// let cc=60;
+// let v=50;
+// console.log(cc<v);
 
 
-let discount = 17.33; 
 
-let Amount = (price*discount) / 100;
-let Total = price - Amount;
-console.log("Customer: " + cusName.toUpperCase());
-console.log("Original Price: LKR " + price.toFixed(2));
-console.log("Discount (" + discount + "%): LKR " + Amount.toFixed(2));
-console.log("Total after discount: LKR " + Total.toFixed(2));
+// let cusName= prompt("Enter Your Name");
+// let price = prompt("Enter the price");
 
-alert(
-  "Hello " + cusName + "!\n" +
-  "Your total after " + discount + "% discount is LKR " + Total.toFixed(2)
-);
+
+// let discount = 17.33; 
+
+// let Amount = (price*discount) / 100;
+// let Total = price - Amount;
+// console.log("Customer: " + cusName.toUpperCase());
+// console.log("Original Price: LKR " + price.toFixed(2));
+// console.log("Discount (" + discount + "%): LKR " + Amount.toFixed(2));
+// console.log("Total after discount: LKR " + Total.toFixed(2));
+
+// alert(
+//   "Hello " + cusName + "!\n" +
+//   "Your total after " + discount + "% discount is LKR " + Total.toFixed(2)
+// );
+
+// classwork
+let Price = 123.4567;
+let rounded = Price.toFixed(2);
+console.log(`Price: Rs ${rounded}`);
+// let fullName = "   jeru  shan   ";
+// let cleanedName = fullName.trim().toUpperCase();
+// console.log(`Hello, ${cleanedName}!`);
+// let item = "Keyboard";
+// let priceText = "Rs 250.00";
+// console.log(`Item: ${item.padEnd(15)} Price: ${priceText}`);
+
 
 
 
